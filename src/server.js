571ace2d10a8714d3e20a3f6ada2c12d31424d91
Ben Smith <benjsmi@us.ibm.com>
2015-11-13 12:50:19 -0500

    Go ahead and deploy roomjs.

--- src/server.js
+++ src/server.js
@@ -1,19 +1,56 @@
-var express = require('express');
-var morgan = require('morgan')
-var app = express();
-var fs = require('fs')
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+var ws = require("nodejs-websocket")
+var http = require("http")
 
-//setup the logger
-app.use(morgan('combined', {stream: accessLogStream}))
+console.log("Registering with the concierge...")
 
-app.get("/", function (req, res) {
-	res.send("The room is up and running.")
-})
+var registration = {
+  roomName: "TheNodeRoom",
+  exits: [
+    {
+      name: "W",
+      room: "RecRoom",
+      description: "You see a door to the west that looks like it goes somewhere."
+    }
+  ],
+  attributes: {
+    endPoint: "ws://game-on.org/roomjs/",
+    startLocation: "false"
+  }
+}
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+console.log(JSON.stringify(registration))
+var options = {
+  host: 'game-on.org',
+  path: '/concierge/registerRoom',
+  method: 'POST',
+  headers: {
+	  'Content-Type':'application/json'
+  }
+};
 
-  console.log('Node app is up and running to serve static HTML content.');
-});
+callback = function(response) {
+  var str = ''
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  response.on('end', function () {
+    console.log("Received response: " + str);
+  });
+}
+var req = http.request(options, callback);
+
+req.write(JSON.stringify(registration));
+req.end();
+
+var wsServer = ws.createServer(function (conn) {
+    conn.on("text", function (str) {
+        console.log("Received "+str)
+        conn.sendText(str.toUpperCase()+"!!!")
+    })
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+    })
+}).listen(3000)
+
+console.log("The WebSocket server is listening...")
