ca507e779c6db0df4abc1e86b9d2f54a2230344a
Ben Smith <benjsmi@us.ibm.com>
2015-12-17 08:58:14 -0500

    Use updated docker configuration.

--- idsBuild.sh
+++ idsBuild.sh
@@ -6,14 +6,19 @@
 
 echo Informing slack...
 curl -X 'POST' --silent --data-binary '{"text":"A new build for the Node.js room has started."}' $WEBHOOK > /dev/null
-mkdir dockercfg ; cd dockercfg
-echo Downloading Docker requirements..
-wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/dockerneeds.tar -q
+
 echo Setting up Docker...
+mkdir dockercfg ; cd dockercfg
+echo -e $KEY > key.pem
+echo -e $CA_CERT > ca.pem
+echo -e $CERT > cert.pem
+cd ..
 wget http://security.ubuntu.com/ubuntu/pool/main/a/apparmor/libapparmor1_2.8.95~2430-0ubuntu5.3_amd64.deb -O libapparmor.deb
 sudo dpkg -i libapparmor.deb
-tar xzf dockerneeds.tar ; mv docker ../ ; cd .. ; chmod +x docker ; \
-	export DOCKER_HOST="tcp://$BUILD_DOCKER_HOST:2376" DOCKER_TLS_VERIFY=1 DOCKER_CONFIG=./dockercfg
+rm libapparmor.deb
+wget https://get.docker.com/builds/Linux/x86_64/docker-1.9.1 --quiet -O docker
+chmod +x docker
+
 
 echo Building the docker image...
 ./docker build -t gameon-room-nodejs .
@@ -22,3 +27,5 @@ echo Stopping the existing container...
 ./docker rm gameon-room-nodejs || true
 echo Starting the new container...
 ./docker run -d -p 5000:3000 -e LOGSTASH_ENDPOINT=$LOGSTASH_ENDPOINT -e LOGSTASH_CERT="$LOGSTASH_CERT" -e LOGSTASH_KEY="$LOGSTASH_KEY" -e CONCIERGE_KEY=$CONCIERGE_KEY --name=gameon-room-nodejs gameon-room-nodejs
+
+rm -rf dockercfg
